c881c1b3b702177f516a57845527929dcd476a88
JiaYi Zhang <bbfag3@gmail.com>
2020-06-03 02:58:10 -0700

    fix format

--- lib/src/ImageMap.dart
+++ lib/src/ImageMap.dart
@@ -29,28 +29,25 @@ class ImageMapState extends State<ImageMap> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTapDown: (details) {
-        RenderBox b = context.findRenderObject();
-        Offset locPos = details.localPosition;
-        double widthMul = widget.imageSize.width / b.size.width;
-        double heightMul = widget.imageSize.height / b.size.height;
-        Offset rawPos = Offset(locPos.dx * widthMul, locPos.dy * heightMul);
-        for (int i = 0; i < widget.regions.length; i++) {
-          if (widget.regions[i].contains(rawPos)) {
-            widget.onTap(i);
-            return;
+        onTapDown: (details) {
+          RenderBox b = context.findRenderObject();
+          Offset locPos = details.localPosition;
+          double widthMul = widget.imageSize.width / b.size.width;
+          double heightMul = widget.imageSize.height / b.size.height;
+          Offset rawPos = Offset(locPos.dx * widthMul, locPos.dy * heightMul);
+          for (int i = 0; i < widget.regions.length; i++) {
+            if (widget.regions[i].contains(rawPos)) {
+              widget.onTap(i);
+              return;
+            }
           }
-        }
-      },
-      child: CustomPaint(
-        child: Image.asset(widget.imagePath),
-        foregroundPainter: ImageMapPainter(
-          shapes: widget.regions,
-          colors: widget.regionColors,
-          rawSize: widget.imageSize,
-          debug: widget.isDebug
-        )
-      )
-    );
+        },
+        child: CustomPaint(
+            child: Image.asset(widget.imagePath),
+            foregroundPainter: ImageMapPainter(
+                shapes: widget.regions,
+                colors: widget.regionColors,
+                rawSize: widget.imageSize,
+                debug: widget.isDebug)));
   }
 }

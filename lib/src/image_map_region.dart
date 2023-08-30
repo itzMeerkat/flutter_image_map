5fe222bc734311228d7ae2a9dbebe12f1a1f1cfc
Jeremy <jeremy@sybrandsplace.nl>
2023-08-30 15:49:32 +0200

    added html parse

--- lib/src/image_map_region.dart
+++ lib/src/image_map_region.dart
@@ -10,27 +10,55 @@ class ImageMapRegion {
     this.link,
   });
 
-  factory ImageMapRegion.fromRect(Rect rect, Color color) {
+  factory ImageMapRegion.fromRect({
+    required Rect rect,
+    Color? color,
+    String? title,
+    String? link,
+  }) {
     return ImageMapRegion(
       shape: ImageMapShape.rect,
       path: Path()..addRect(rect),
-      color: color,
+      color: color ?? Colors.transparent,
+      title: title,
+      link: link,
     );
   }
 
-  factory ImageMapRegion.fromPoly(List<Offset> points, Color color) {
+  factory ImageMapRegion.fromPoly({
+    required List<Offset> points,
+    Color? color,
+    String? title,
+    String? link,
+  }) {
     return ImageMapRegion(
       shape: ImageMapShape.poly,
       path: Path()..addPolygon(points, true),
-      color: color,
+      color: color ?? Colors.transparent,
+      title: title,
+      link: link,
     );
   }
 
-  factory ImageMapRegion.fromCircle(Offset center, double radius, Color color) {
+  factory ImageMapRegion.fromCircle({
+    required Offset center,
+    required double radius,
+    Color? color,
+    String? title,
+    String? link,
+  }) {
     return ImageMapRegion(
       shape: ImageMapShape.circle,
-      path: Path()..addOval(Rect.fromCircle(center: center, radius: radius)),
-      color: color,
+      path: Path()
+        ..addOval(
+          Rect.fromCircle(
+            center: center,
+            radius: radius,
+          ),
+        ),
+      color: color ?? Colors.transparent,
+      title: title,
+      link: link,
     );
   }
 
@@ -39,4 +67,105 @@ class ImageMapRegion {
   final Color color;
   final String? title;
   final String? link;
+
+  static List<ImageMapRegion> fromHtml(
+    String data, [
+    Color? color,
+  ]) {
+    final regions = <ImageMapRegion>[];
+    final areaMatches = RegExp(
+      r'<area .+>',
+      caseSensitive: false,
+      multiLine: true,
+    ).allMatches(data);
+
+    final shapeRegex = RegExp(
+      r'shape="([^"]+)"',
+      caseSensitive: false,
+    );
+    final coordsRegex = RegExp(
+      r'coords="([^"]+)"',
+      caseSensitive: false,
+    );
+    final titleRegex = RegExp(
+      r'title="([^"]+)"',
+      caseSensitive: false,
+    );
+    final linkRegex = RegExp(
+      r'href="([^"]+)"',
+      caseSensitive: false,
+    );
+
+    for (final areaMatch in areaMatches) {
+      final areaValue = areaMatch.group(0);
+      if (areaValue == null) {
+        continue;
+      }
+      final shapeMatch = shapeRegex.firstMatch(areaValue);
+      final coordsMatch = coordsRegex.firstMatch(areaValue);
+      if (shapeMatch == null || coordsMatch == null) {
+        continue;
+      }
+
+      final titleMatch = titleRegex.firstMatch(areaValue);
+      final linkMatch = linkRegex.firstMatch(areaValue);
+
+      final shape = ImageMapShape.fromString(shapeMatch.group(1)!);
+      final coords = coordsMatch
+          .group(1)!
+          .split(',')
+          .map(double.parse)
+          .toList(growable: false);
+
+      switch (shape) {
+        case ImageMapShape.rect:
+          regions.add(
+            ImageMapRegion.fromRect(
+              rect: Rect.fromLTRB(
+                coords[0],
+                coords[1],
+                coords[2],
+                coords[3],
+              ),
+              color: color,
+              title: titleMatch?.group(1),
+              link: linkMatch?.group(1),
+            ),
+          );
+          break;
+        case ImageMapShape.poly:
+          final offsets = <Offset>[];
+          for (var i = 0; i < coords.length; i += 2) {
+            final x = coords[i];
+            final y = coords[i + 1];
+            offsets.add(Offset(x, y));
+          }
+          regions.add(
+            ImageMapRegion.fromPoly(
+              points: offsets,
+              color: color,
+              title: titleMatch?.group(1),
+              link: linkMatch?.group(1),
+            ),
+          );
+          break;
+        case ImageMapShape.circle:
+          regions.add(
+            ImageMapRegion.fromCircle(
+              center: Offset(
+                coords[0],
+                coords[1],
+              ),
+              radius: coords[2],
+              color: color,
+              title: titleMatch?.group(1),
+              link: linkMatch?.group(1),
+            ),
+          );
+          break;
+      }
+    }
+
+    return regions;
+  }
 }
--- lib/src/image_map_shape.dart
+++ lib/src/image_map_shape.dart
@@ -2,4 +2,8 @@ enum ImageMapShape {
   rect,
   poly,
   circle;
+
+  static ImageMapShape fromString(String name) {
+    return ImageMapShape.values.where((element) => element.name == name).first;
+  }
 }

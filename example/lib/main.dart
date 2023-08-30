69ae4e60a764e16facf02ec797d6ef64b54dab12
Jeremy <jeremy@sybrandsplace.nl>
2023-08-30 14:05:02 +0200

    removed commented code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,33 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class ImageMapExample extends State<MyApp> {
-  // static List<List<Offset>> points = [
-  //   [
-  //     const Offset(178, 152),
-  //     const Offset(148, 179),
-  //     const Offset(125, 173),
-  //     const Offset(129, 191),
-  //     const Offset(87, 191),
-  //     const Offset(130, 226),
-  //     const Offset(121, 270),
-  //     const Offset(182, 285),
-  //     const Offset(185, 272),
-  //     const Offset(219, 276),
-  //     const Offset(239, 260),
-  //     const Offset(218, 225),
-  //     const Offset(245, 186),
-  //   ]
-  // ];
-  // final List<Path> polygonRegions = points.map((e) {
-  //   Path p = Path();
-  //   p.addPolygon(e, true);
-  //   return p;
-  // }).toList();
-  // final List<Color> colors = List.generate(
-  //   points.length,
-  //   (index) => const Color.fromRGBO(50, 50, 200, 0.5),
-  // );
-
   bool _tapped = false;
 
   @override
